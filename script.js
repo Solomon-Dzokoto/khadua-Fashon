7fd709dd8037cd6ef2dac4a39c2be020a074e67f
Solomon-Dzokoto <dzokotosolomon85@gmail.com>
2024-07-25 03:07:52 +0000

    grand finale

--- script.js
+++ script.js
@@ -12,3 +12,66 @@ closeNav.addEventListener('click',()=>{
   openNav.style.display="grid";
   closeNav.style.display="none";
 })
+
+const ScrollRevealOption = {
+  distance:"3rem",
+  origin:"bottom",
+  interval:500,
+  duration:2000,
+};
+
+ScrollReveal().reveal(".first__section .container .image img", {
+  ...ScrollRevealOption,
+  origin: "right",
+  distance: "5rem",
+  duration: 1000,
+});
+ScrollReveal().reveal(".first__section .container .image img", {
+  ...ScrollRevealOption,
+  origin: "right",
+  distance: "5rem",
+  duration: 1000,
+});
+ScrollReveal().reveal(".first__section .container .header", {
+  ...ScrollRevealOption,
+  origin: "left",
+  distance: "5rem",
+  duration: 2000,
+});
+ScrollReveal().reveal(".first__section .container .header h1", {
+  ...ScrollRevealOption,
+  origin: "bottom",
+  distance: "6rem",
+  duration: 2500,
+});
+ScrollReveal().reveal(".first__section .container .header p", {
+  ...ScrollRevealOption,
+  origin: "bottom",
+  distance: "8rem",
+  duration: 3500,
+});
+ScrollReveal().reveal("article", {
+  ...ScrollRevealOption,
+  origin: "bottom",
+  distance: "8rem",
+ interval:500,
+});
+ScrollReveal().reveal(".about__us .top h1", {
+  ...ScrollRevealOption,
+  origin: "left",
+  distance: "8rem",
+ interval:500,
+});
+ScrollReveal().reveal(".about__us .bottom .right .image", {
+  ...ScrollRevealOption,
+  origin: "right",
+  distance: "8rem",
+duration:1000,
+});
+ScrollReveal().reveal(".about__us .top p", {
+  ...ScrollRevealOption,
+  origin: "left",
+  distance: "10rem",
+  delay:300,
+duration:3000,
+});
